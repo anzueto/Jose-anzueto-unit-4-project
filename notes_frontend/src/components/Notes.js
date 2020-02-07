0bf5181b9839fceda6f564703002f40cbf04c7f5
anzueto <jose@C4-1152.local>
2020-02-07 00:01:21 -0500

    created my Notes.js in components

---
+++ notes_frontend/src/components/Notes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Notes extends Component {
+    render() {
+        return (
+            <h1>Notes Reminders</h1>
+        )
+    }
+}
+
+export default Notes;

39fcbe6ea6f41418fff7df16d3aba0453c8ee8ac
anzueto <jose@C4-1152.local>
2020-02-07 00:16:48 -0500

    added componentDidMount, and fetch

--- notes_frontend/src/components/Notes.js
+++ notes_frontend/src/components/Notes.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class Notes extends Component {
+
+    state = {
+        notes: []
+    }
+
+    componentDidMount() {
+        this.getNotes()
+    }
+
+    getNotes = () => {
+        fetch('http://localhost:3000/notes')
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(error => console.log(error))
+    }
+
+
     render() {
         return (
             <h1>Notes Reminders</h1>
